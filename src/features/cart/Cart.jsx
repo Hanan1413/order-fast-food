45a69583af75f9ff2699fbf65e45aaa205ecd601
Hanan Ibrahim <ihanan977@gmail.com>
2024-08-01 10:44:19 +0300

    update cart and menu components

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import LinkButton from '../../ui/LinkButton';
-import Button from '../../ui/Button'
+import Button from '../../ui/Button';
+import CartItem from '../cart/CartItem';
 
 const fakeCart = [
   {
@@ -30,18 +31,25 @@ function Cart() {
   const cart = fakeCart;
 
   return (
-    <div>
+    <div className="px-4 py-3">
       <LinkButton to="/menu">
         &larr; Back to menu
       </LinkButton>
 
-      <h2 className='mb-4'>Your cart, %NAME%</h2>
+      <h2 className='mt-7 tex-xl font-semibold mb-2 '>Your cart, %NAME%</h2>
+      <ul className="divide-y divide-stone-200 mb-4 border-b pb-8">
+        {cart.map((item)=>{
+          return <CartItem  item={item} key={item.key}/>
+        })}
+      </ul>
 
-      <div>
+
+
+      <div className="space-x-4">
         <Button to="/order/new" >Order pizzas</Button>
 
         {/* <Link to=></Link> */}
-        <button>Clear cart</button>
+        <Button type="secondary">Clear cart</Button>
       </div>
     </div>
   );
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,10 +1,17 @@
 import {Link} from 'react-router-dom';
 
-function Button({ children, disabled, to  }) {
+function Button({ children, disabled, to, type }) {
   const className = "mt-3 bg-yellow-500 p-2 uppercase  tracking-wide rounded-full ml-3 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring-2 focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+  const  base = "mt-3 bg-yellow-500 p-2 uppercase  tracking-wide rounded-full ml-3 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring-2 focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+  
+  const styles = {
+   primary: base + " px-4  py-3 md:px-6 md:py-4",
+   small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+   secondary:"inline-block rounded-full boder-2-stone-300 mt-3 bg-yellow-500 p-2 uppercase  tracking-wide rounded-full ml-3 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring-2 focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed  px-4  py-3 md:px-6 md:py-4",
+  }   
 
   if(to)
-    return <Link to={to} className={className}>
+    return <Link to={to} className={className} style={styles[type]}>
      {children}
     </Link>
   return (
